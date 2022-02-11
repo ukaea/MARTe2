9042ee6de4d3cb5e33b8feafa5d3d62a21790971
Colin.Hogben <Colin.Hogben@ukaea.uk>
2022-02-11 17:41:35 +0000

    Linux/MutexSem: Avoid use of deprecated ftime
    Use clock_gettime(CLOCK_REALTIME, ) instead of ftime, which was deprecated
    in libc 2.31, and will be removed from a future version of libc.

--- Source/Core/Scheduler/L1Portability/Environment/Linux/MutexSem.cpp
+++ Source/Core/Scheduler/L1Portability/Environment/Linux/MutexSem.cpp
@@ -229,23 +229,18 @@ ErrorManagement::ErrorType MutexSem::Lock(const TimeoutType &timeout) {
     }
     else {
         if (ok) {
-            struct timespec timesValues;
-            timeb tb;
-            ok = (ftime(&tb) == 0);
-
+            struct timespec timeNow;
+            ok = (clock_gettime(CLOCK_REALTIME, &timeNow) == 0);
             if (ok) {
-                float64 sec = static_cast<float64>(timeout.GetTimeoutMSec());
-                sec += static_cast<float64>(tb.millitm);
-                sec *= 1e-3;
-                sec += static_cast<float64>(tb.time);
-
-                float64 roundValue = floor(sec);
-                timesValues.tv_sec = static_cast<int32>(roundValue);
-                float64 nSec = (sec - roundValue);
-                nSec *= 1e9;
-                timesValues.tv_nsec = static_cast<int32>(nSec);
-
-                ok = (pthread_mutex_timedlock(&handle->mutexHandle, &timesValues) == 0);
+                struct timespec timeEnd = timeNow;
+                uint64 usec = timeout.GetTimeoutUSec();
+                timeEnd.tv_sec += usec / 1000000;
+                timeEnd.tv_nsec += (usec % 1000000) * 1000;
+                if (timeEnd.tv_nsec >= 1000000000) {
+                    timeEnd.tv_nsec -= 1000000000;
+                    timeEnd.tv_sec += 1;
+                }
+                ok = (pthread_mutex_timedlock(&handle->mutexHandle, &timeEnd) == 0);
                 if (!ok) {
                     err = ErrorManagement::Timeout;
                     REPORT_ERROR_STATIC_0(err, "Information: timeout occurred");
@@ -255,7 +250,7 @@ ErrorManagement::ErrorType MutexSem::Lock(const TimeoutType &timeout) {
                 }
             }
             else {
-                REPORT_ERROR_STATIC_0(ErrorManagement::OSError, "Error: ftime()");
+                REPORT_ERROR_STATIC_0(ErrorManagement::OSError, "Error: clock_gettime()");
             }
         }
         else {
